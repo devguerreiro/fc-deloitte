e0902ecceb53c0daa68545ffcc319d70b8b1eff0
Luis Guerreiro <luiscvlh11@gmail.com>
2023-10-30 19:59:10 -0300

    feat: :sparkles: adicionado conteudo a pagina inicial

--- gpapp/src/pages/index.vue
+++ gpapp/src/pages/index.vue
@@ -5,5 +5,15 @@ definePageMeta({
 </script>
 
 <template>
-    <h1>hello world</h1>
+    <div class="t-flex t-items-center t-flex-col t-gap-20">
+        <h1 class="t-text-4xl t-font-bold t-self-start">
+            Bem vindo ao sistema GPAPP
+        </h1>
+        <img
+            src="/high-school.jpg"
+            width="500"
+            height="500"
+            alt="Ilustracao de um colegio com alguns estudantes na frente"
+        />
+    </div>
 </template>
